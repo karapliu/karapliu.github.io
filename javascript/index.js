aabf3ef1aaf16e3291a060b605ae0e06615cc214
kara <karaliuu@gmail.com>
2020-05-08 01:31:11 +0800

    Change small screen project section

--- javascript/index.js
+++ javascript/index.js
@@ -248,7 +248,7 @@ const pickle = () => {
     bobudget.classList.remove("active");
   }
 
-  mac.innerHTML = "<a id='link' target='_blank' href='https://pickle-gpig-app.herokuapp.com/#/'>Visit the Site</a><img src='assets/images/projects/pickle.gif'>";
+  mac.innerHTML = "<a class='link' target='_blank' href='https://pickle-gpig-app.herokuapp.com/#/'>Visit the Site</a><img src='assets/images/projects/pickle.gif'>";
   desc.innerHTML = "<p><a target='_blank' href='https://pickle-gpig-app.herokuapp.com/#/'>Pickle</a>, a pixel-perfect clone of Rover, is an online marketplace that allows users to buy and sell guinea pig services.</p><div class='tech-n-links'><div class='i-links'><a target='_blank' href='https://pickle-gpig-app.herokuapp.com/#/'><i class='far fa-window-restore'></i></a><a target='_blank' href='https://github.com/karapliu/pickle-app'><i class='fab fa-github'></i></a></div><div class='technologies'><h6>React/Redux</h6><h6>Ruby on Rails</h6><h6>JavaScript</h6><h6>PostgreSQL</h6><h6>Amazon AWS</h6></div></div>";
 
   pickle.classList.add("active");
@@ -268,7 +268,7 @@ const wiggleAnimals = () => {
     bobudget.classList.remove("active");
   }
 
-  mac.innerHTML = "<a id='link' target='_blank' href='https://karapliu.github.io/wiggle-animals/'>Visit the Site</a><img src='assets/images/projects/wiggle-animals.gif'>";
+  mac.innerHTML = "<a class='link' target='_blank' href='https://karapliu.github.io/wiggle-animals/'>Visit the Site</a><img src='assets/images/projects/wiggle-animals.gif'>";
   desc.innerHTML = "<p><a target='_blank' href='https://karapliu.github.io/wiggle-animals/'>Wiggle Animals</a> is a fun and interactive website where users can interact with animals through mouse hovers. All animal components are created with HTML5 canvas.</p><div class='tech-n-links'><div class='i-links'><a target='_blank' href='https://karapliu.github.io/wiggle-animals/'><i class='far fa-window-restore'></i></a><a target='_blank' href='https://github.com/karapliu/wiggle-animals'><i class='fab fa-github'></i></a></div><div class='technologies'><h6>Javascript</h6><h6>HTML5 Canvas</h6><h6>SCSS</h6></div></div>";
   wiggle.classList.add("active");
 }
@@ -287,7 +287,7 @@ const bobudget = () => {
     wiggle.classList.remove("active");
   }
 
-  mac.innerHTML = "<a id='link' target='_blank' href='http://bobudget.herokuapp.com/#/'>Visit the Site</a><img src='assets/images/projects/bobudget.gif'>";
+  mac.innerHTML = "<a class='link' target='_blank' href='http://bobudget.herokuapp.com/#/'>Visit the Site</a><img src='assets/images/projects/bobudget.gif'>";
   desc.innerHTML = "<p><a target='_blank' href='http://bobudget.herokuapp.com/#/'>BoBudget</a> is a creative app where users can track their boba spendings. Other functionalities include a boba-themed dashboard, and a search for nearby stores.</p><div class='tech-n-links'><div class='i-links'><a target='_blank' href='http://bobudget.herokuapp.com/#/'><i class='far fa-window-restore'></i></a><a target='_blank' href='https://github.com/karapliu/bobudget-app'><i class='fab fa-github'></i></a></div><div class='technologies'><h6>MongoDB</h6><h6>Express</h6><h6>React</h6><h6>node.js</h6><h6>Amazon AWS</h6></div></div>";
   bobudget.classList.add("active");
 }
